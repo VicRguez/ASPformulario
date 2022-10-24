bbc416ae0c7bbf6233f0c6667889c3b5ab199917
INT\Victoria.Rodriguez <victoria.rodriguez@solera.com>
2022-10-24 12:53:53 +0200

    Se añade todo

--- ASPPrueba/Migrations/20221024102039_addPreguntastabla.Designer.cs
+++ ASPPrueba/Migrations/20221024102039_addPreguntastabla.Designer.cs
@@ -4,6 +4,7 @@ using ASPPrueba.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ASPPrueba.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20221024102039_addPreguntastabla")]
+    partial class addPreguntastabla
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
