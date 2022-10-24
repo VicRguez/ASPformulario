cb81749fd86c315ee288a0b3826202f7452b12d7
INT\Victoria.Rodriguez <victoria.rodriguez@solera.com>
2022-10-24 10:01:16 +0200

    first commit

---
+++ ASPPrueba/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using ASPPrueba.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPPrueba.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category>categories { get; set; }  
+    }
+}
