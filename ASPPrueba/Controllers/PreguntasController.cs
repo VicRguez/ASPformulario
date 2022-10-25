d00d61be92f6402d0e82093dae4738806984eb85
Puerto Manuel <manuel.puerto@solera.com>
2022-10-25 12:32:54 +0200

    se añade la vista de la primera pregunta

--- ASPPrueba/Controllers/PreguntasController.cs
+++ ASPPrueba/Controllers/PreguntasController.cs
@@ -3,6 +3,7 @@
 using ASPPrueba.Data;
 using ASPPrueba.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 public class PreguntasController : Controller
 {
@@ -16,8 +17,23 @@ public class PreguntasController : Controller
     public IActionResult ListadoPreguntas()
     {
         IEnumerable<Pregunta> objPreguntasList = _db.preguntas;
+        objPreguntasList.ElementAt(0);
         return View(objPreguntasList);
     }
+
+    public IActionResult PrimeraPregunta()
+    {
+        IEnumerable<Pregunta> objPreguntasList = _db.preguntas;
+
+        List<Pregunta> asList = objPreguntasList.ToList();
+
+        Pregunta primeraPregunta =  asList.ElementAt(0);
+
+   
+
+        return View(primeraPregunta);
+    }
+
 
 
 }
