0062d8e569e4798f8ea52e0e93143d7c609b52f6
VicRguez <95369668+VicRguez@users.noreply.github.com>
2022-10-26 16:11:15 +0200

    adding tests for the list of questions

---
+++ com.Victoria.seleniumtest/src/test/java/TestingVictoria.java
@@ -0,0 +1,38 @@
+package com.Victoria.seleniumtest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestingVictoria {
+
+	@Test
+	public void test() {
+		WebDriverManager.chromedriver().setup();
+		
+
+
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://localhost:7211/");
+		
+		driver.findElement(By.xpath("/html/body/header/nav/div/div/ul/li[3]/a"))
+		.click();
+		
+		driver.findElement(By.xpath("/html/body/div/main/table/tbody/tr[1]/td[3]/div[1]/a"))
+		.click();
+		
+		String actualUrl="https://localhost:7211/Preguntas/Create/1";
+
+		String expectedUrl= driver.getCurrentUrl();
+
+		Assert.assertEquals(expectedUrl,actualUrl);
+	}
+
+}
